d6c8c686611a75bd331fde43a80aff848798be1d
lepesh <nelepesh100@mail.ru>
2024-01-31 23:59:01 +0300

    add FileSystemConstants and VcsRootDirectoryNavigator

---
+++ YAVCS/Constants/FileSystemConstants.cs
@@ -0,0 +1,37 @@
+﻿namespace YAVCS.Constants;
+/*!
+  \file   FileSystemConstants.cs
+  \brief  File with general folders/files names
+  
+  Vcs root Directory Format:
+  .yavcs/
+    refs/
+      heads/
+      tags/
+    objects/
+      pack/
+    index 
+    head
+    config
+      
+  \author lepesh
+  \date   31.01.2024
+*/
+
+public static class FileSystemConstants
+{
+    // root folder name 
+    public const string VcsRootDirectory = ".yavcs";
+    // refs folder name
+    public const string RefsDirectory = "refs";
+    // heads(branches) folder name
+    public const string HeadsDirectory = "heads";
+    // objects folder name
+    public const string ObjectsDirectory = "objects";
+    // index file name
+    public const string IndexFile = "index";
+    // head file name
+    public const string HeadFile = "head";
+    // config File name
+    public const string ConfigFile = "config";
+}
---
+++ YAVCS/VcsRootDirectoryNavigator/VcsRootDirectoryNavigator.cs
@@ -0,0 +1,30 @@
+﻿using YAVCS.Constants;
+
+namespace YAVCS.VcsRootDirectoryNavigator;
+
+/*!
+  \file   VcsRootDirectoryNavigator.cs
+  \brief  Class with VCS root directory navigation properties 
+
+  primary constructor take 1 argument - absolute path to repository root directory
+  Root directory format in FileSystemConstants.cs description
+
+  \author lepesh
+  \date   31.01.2024
+*/
+
+public class VcsRootDirectoryNavigator(string absoluteRepositoryPath)
+{
+    public string VcsRootDirectory => absoluteRepositoryPath + '/' + FileSystemConstants.VcsRootDirectory;
+    public string RefsDirectory => VcsRootDirectory + '/' + FileSystemConstants.RefsDirectory;
+
+    public string HeadsDirectory => RefsDirectory + '/' + FileSystemConstants.HeadsDirectory;
+
+    public string ObjectsDirectory => VcsRootDirectory + '/' + FileSystemConstants.ObjectsDirectory;
+
+    public string IndexFile => VcsRootDirectory + '/' + FileSystemConstants.IndexFile;
+
+    public string HeadFile => VcsRootDirectory + '/' + FileSystemConstants.HeadFile;
+
+    public string ConfigFile => VcsRootDirectory + '/' + FileSystemConstants.ConfigFile;
+}
