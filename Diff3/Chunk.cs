29325d204714ff6ffaa097698a9541a0253deda7
lepesh <nelepesh100@mail.ru>
2024-05-02 17:06:32 +0300

    Diff3 add

--- Diff3/Chunk.cs
+++ Diff3/Chunk.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Verano.Diff3Way;
 
-namespace Verano.Diff3Way
+namespace Diff3
 {
     public class Chunk
     {
